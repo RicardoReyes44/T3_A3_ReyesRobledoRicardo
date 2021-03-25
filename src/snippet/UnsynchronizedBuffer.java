2e8b9e78500c42427a8811f92e58d4123be94d54
RicardoReyes44 <rickyboy1002@gmail.com>
2021-03-25 15:42:03 -0600

    Se agrego el atributo 'buffer' de tipo entero y los metodos 'blockingPut' y 'blockingGet' en la clase 'UnsynchronizedBuffer'

--- src/snippet/UnsynchronizedBuffer.java
+++ src/snippet/UnsynchronizedBuffer.java
@@ -1,5 +1,19 @@
 package snippet;
 
-public class UnsynchronizedBuffer {
+public class UnsynchronizedBuffer implements Buffer{
 
-}
+	private int buffer = -1;
+	
+	@Override
+	public void blockingPut(int value) throws InterruptedException {
+		System.out.printf("Producer writes\t%2d", value);
+		buffer = value;
+	}
+
+	@Override
+	public int blockingGet() throws InterruptedException {
+		System.out.printf("Consumer reads\t%2d", buffer);
+		return buffer;
+	}
+	
+}//UnsynchronizedBuffer
